89927df25bcfc550e3b0b748395ab5b8c9c1e05e
CCDM17 <ccdm17@CCDM17-2.local>
2022-11-09 15:55:23 -0700

    Primera practica Vue.js

---
+++ randomuser_vue/main.js
@@ -0,0 +1,25 @@
+const App = {
+    data:function(){
+        return{
+            mensaje:'Hola Vue.js',
+            muestra:false,
+            nombre:''
+        }
+    },
+    methods:{
+        cambiarMuestra:function(){
+            this.muestra=!this.muestra
+        },
+        randomUser:async function(){
+            let n=""
+            await axios.get('https://randomuser.me/api/')
+            .then(function(response){
+                // console.log(response.data.results[0].name.last)
+                n=response.data.results[0].name.last
+            });
+            this.nombre=n
+        }
+    }
+};
+
+Vue.createApp(App).mount("#app");
